f51a6382f872fc28839bad445da95b3d2ed8773a
iamharshkumar <hk7206210@gmail.com>
2020-06-14 12:38:21 +0530

    add similar movies using pandas ,change type and category choices, add quality field in episode model

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import MovieListView, SearchView, MovieDetail, GenreFetch, GenreList, FetchCategoryType
 
 urlpatterns = [
-    path('movie', MovieListView.as_view()),
+    # path('movie', MovieListView.as_view()),
     path('search', SearchView.as_view(), name='search-title'),
-    path('movie/<pk>', MovieDetail.as_view(), name='movie-detail'),
+    path('movie', MovieDetail.as_view(), name='movie-detail'),
     path('genre/search', GenreFetch.as_view(), name='genre-fetch'),
     path('genre/list', GenreList.as_view(), name='genre-list'),
     path('fetch/', FetchCategoryType.as_view(), name='fetch-movie'),
